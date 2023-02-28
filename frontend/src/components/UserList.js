61e41ab89f54d3edfc6d34dc92ddf43088883b49
Shikhar Mishra <68225661+shkhrtrxsh@users.noreply.github.com>
2023-02-28 16:08:13 +0530

    Update UserList.js

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const BASE_URL= "https://todo-list-app-production-0e0f.up.railway.app/"
+const BASE_URL= "https://todo-list-app-production-0e0f.up.railway.app"
 export const UserList = () => {
   const [userData, setUserData] = useState(null);
 
