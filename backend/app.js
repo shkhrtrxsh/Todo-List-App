8b538f3c64de1f434741f92df32f3d848c94270c
Shikhar Mishra <68225661+shkhrtrxsh@users.noreply.github.com>
2023-02-28 18:08:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const connectToDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
 
+// Add CORS middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
