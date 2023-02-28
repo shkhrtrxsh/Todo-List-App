2279619a1d07f1b1974d454408a9a827eae394b7
Shikhar Mishra <68225661+shkhrtrxsh@users.noreply.github.com>
2023-02-28 15:52:00 +0530

    Add files via upload

---
+++ frontend/src/config.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDU8Qz5YzPTT4j9Pb7i0nNIWxY46rLWHro",
+  
+    authDomain: "todo-app-6fef8.firebaseapp.com",
+  
+    projectId: "todo-app-6fef8",
+  
+    storageBucket: "todo-app-6fef8.appspot.com",
+  
+    messagingSenderId: "711750802665",
+  
+    appId: "1:711750802665:web:e4f0ae3c055fc43b222478",
+  
+    measurementId: "G-7XZFWR7T91"
+  
+  };
+export default firebaseConfig
