73774524ab6170ae548dc6fe997fe80282c802e4
Shikhar Mishra <68225661+shkhrtrxsh@users.noreply.github.com>
2023-02-28 19:10:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,7 @@ const app = express();
 // Add CORS middleware
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
+  res.setHeader('Allow', 'OPTIONS, GET, POST, PUT, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
