4a4ac156db12ef95b4ef84b15f568edd134c435d
Shikhar Mishra <shkhrtrxsh@gmail.com>
2023-02-28 14:39:23 +0530

    first commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "Name is Required"],
+    trim: true,
+    maxlength: [25, "Name must be 25 Ch Long"],
+  },
+  email: {
+    type: String,
+    require: [true, "Email is Required"],
+    unique: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+}
+});
+
+module.exports = mongoose.model("User", userSchema);
